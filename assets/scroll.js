d2ade56b2cea8abd4868393b477e0d8c3419099d
storyandfortune@github.com <fortune@storyandfortune.com>
2021-09-22 13:58:13 -0700

    add footer scroll animation & headr updates

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,1 +1,28 @@
 /* here's where we use waypoints */
+
+
+$(document).ready(function () {
+
+    /* footer big boy coming up */
+    var BigBoyBottom = $('#big-boy-footer').waypoint(function (direction) {
+
+        if (direction === "down") {
+            $('#big-boy-footer').addClass('up');
+        }
+        else {
+            $('#big-boy-footer').removeClass('up');
+        }
+
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+    $('#big-boy-footer').bind('click', function () {
+        $('#big-boy-footer').toggleClass('up');
+    });
+
+    /* ------------------------------------------ */
+
+});
+
+
