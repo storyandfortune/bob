ee82a461997c7a5bf338ff741de36d65c5c3c202
storyandfortune@github.com <fortune@storyandfortune.com>
2021-10-05 10:07:24 -0700

    upadating styles for hero-scroll & secondary pages

--- assets/big-boy-time.js
+++ assets/big-boy-time.js
@@ -3,44 +3,37 @@
 
 $(document).ready(function () {
 
-
-
     /* Waypoints --------------------------------------------------------- */
 
-    /* sticky header */
-    var StickyHeader = $('#header-top').waypoint(function (direction) {
+    if ($('body').hasClass('home-page')) {
 
+        /* sticky header */
+        var StickyHeader = $('#header-top').waypoint(function (direction) {
 
-        if (direction === "down") {
-            console.log('add sticky');
-            $('#shopify-section-header').addClass('sticky');
-        }
-        else {
-            console.log('remove sticky');
-            $('#shopify-section-header').removeClass('sticky');
-        }
+            if (direction === "down") {
+                $('#shopify-section-header').addClass('sticky');
+            }
+            else {
+                $('#shopify-section-header').removeClass('sticky');
+            }
 
-    });
+        });
 
+        /* big boy hides */
 
+        var BigBoyHides = $('#big-boy-header').waypoint(function (direction) {
 
-    /* big boy hides */
+            if (direction === "down") {
+                $('#big-boy-header').addClass('hide');
+            }
+            else {
+                $('#big-boy-header').removeClass('hide');
+            }
 
-    var BigBoyHides = $('#big-boy-header').waypoint(function (direction) {
-
-        if (direction === "down") {
-            $('#big-boy-header').addClass('hide');
-        }
-        else {
-            $('#big-boy-header').removeClass('hide');
-        }
-
-    }, {
-        offset: '65%'
-    });
-
-
-
+        }, {
+            offset: '65%'
+        });
+    }
 
     /* footer big boy coming up */
     var BigBoyBottom = $('#big-boy-footer').waypoint(function (direction) {
@@ -55,12 +48,6 @@ $(document).ready(function () {
     }, {
         offset: 'bottom-in-view'
     });
-
-
-
-
-
-
 
 
     $('#big-boy-footer').bind('click', function () {
