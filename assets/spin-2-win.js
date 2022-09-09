04c15858cac7d0f673891123e210afe25392b5d4
storyandfortune@github.com <fortune@storyandfortune.com>
2022-09-09 08:46:01 -0700

    updating google TOS, PP some spin to win updates

--- assets/spin-2-win.js
+++ assets/spin-2-win.js
@@ -1,18 +1,41 @@
-function spin(){
+let spin = function(){
 
-	var x = 1024; //min value
-	var y = 9999; // max value
+	let win = [
+		{deg:0, title:"20% Off", img:""}, 
+		{deg:90, title:"10% Off", img:""},
+		{deg:180, title:"Free Shipping", img:""},
+		{deg:270, title:"Free Spice", img:""}
+	];
 
-	var deg = Math.floor(Math.random() * (x - y)) + y;
+	let copy = {
+		defaultTitle:"Spin to Win",
+		defualttagline:"Spin the wheel for a chance to win!",
+		cta:"Enter your e-mail address to collect your prize."
+	}
 
-	document.getElementById('box').style.transform = "rotate("+deg+"deg)";
-	console.log(deg / 360);
+	let prize = (Math.floor(Math.random() * (1 - win.length)) + win.length) -1;
 
+	let x = 5; //min rotation
+	let y = 25; // max rotation
 
-	var element = document.getElementById('mainbox');
-	element.classList.remove('animate');
+	let deg = 360 * (Math.floor(Math.random() * (x - y)) + y) + win[prize].deg;
 
-	setTimeout(function(){
-		element.classList.add('animate');
-	}, 5000); //5000 = 5 second
+	$('#box').css("transform", "rotate("+deg+"deg)");
+
+	setTimeout(() => {
+		$('#title h2').html("You won, " + win[prize].title +"!");
+		$('#title span').html(copy.cta);
+		$('#spin').addClass('shrink');
+	}, 5500);
+
+}
+
+// submit e-mail.
+let win = function(){
+
+}
+
+// reset game.
+let reset = function(){
+
 }
