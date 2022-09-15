302d917578f9f2d6855d359c88ebf5618c497da1
storyandfortune@github.com <fortune@storyandfortune.com>
2022-09-14 17:21:23 -0700

    more spin updates

--- assets/spin-2-win.js
+++ assets/spin-2-win.js
@@ -1,15 +1,27 @@
-let spin = function(){
+
 
 	let win = [
-		{deg:0, title:"Beef Patty", img:"", code:"WIN25%"}, 
-		{deg:45, title:"25% Off", img:"", code:"WIN25%"}, 
-		{deg:90, title:"Free Stickers", img:"", code:"WIN20%OFF"},
-		{deg:135, title:"20% Off", img:"", code:"WIN20%OFF"},
-		{deg:180, title:"Free Postcards", img:"", code:"XXX"},
-		{deg:225, title:"10% Off", img:"", code:"WIN10%OFF"},
-		{deg:270, title:"Free Emoji", img:"", code:"WIN15%OFF"},
-		{deg:315, title:"15% Off", img:"", code:"WIN15%OFF"},
+		{deg:0, title:"Beef Patty", img:"", code:"", ratio:0}, 
+		{deg:45, title:"25% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-25percent.png?v=1663197700", code:"WIN25%OFF", ratio:10}, 
+		{deg:90, title:"Free Stickers", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-stickers.png?v=1663197700", code:"FREESTICKER", ratio:100 },
+		{deg:135, title:"20% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-20percent.png?v=1663197699", code:"WIN20%OFF", ratio:5 },
+		{deg:180, title:"Free Postcards", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-postcards.png?v=1663197699", code:"FREEPOSTCARDS", ratio:100},
+		{deg:225, title:"10% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-10percent.png?v=1663197699", code:"WIN10%OFF", ratio:100},
+		{deg:270, title:"Free Emoji", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-emoji.png?v=1663197699", code:"FREEEMOJI", ratio:100},
+		{deg:315, title:"15% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-15percent.png?v=1663197699", code:"WIN15%OFF", ratio:100}
 	];
+
+	let win_ratio = [];
+
+	win.forEach((element) => {
+
+		for(i=0; i<element.ratio; i++){
+			win_ratio.push(element);
+		}
+
+	});
+
+let spin = function(){
 
 	let copy = {
 		defaultTitle:"Spin to Win",
@@ -17,31 +29,27 @@ let spin = function(){
 		cta:"Enter your e-mail address to collect your prize."
 	}
 
-	let prize = (Math.floor(Math.random() * (1 - win.length)) + win.length) -1;
-	console.log(win[prize]);
-
+	let prize = (Math.floor(Math.random() * (1 - win_ratio.length)) + win_ratio.length) -1;
+	let winning_prize = win_ratio[prize];
 	let x = 5; //min rotation
 	let y = 25; // max rotation
 
-	let deg = 360 * (Math.floor(Math.random() * (x - y)) + y) + win[prize].deg;
-
-	//$('#box').css("transform", "rotate("+deg+"deg)");
+	let deg = 360 * (Math.floor(Math.random() * (x - y)) + y) + winning_prize.deg;
 
 	$('#box').css("transform", "rotate(-"+deg+"deg)");
 
 	setTimeout(() => {
-		$('#title h2').html( win[prize].title +"!");
+		$('#title h2').html(winning_prize.title +"!");
 		$('#title span').html(copy.cta);
-		//$('#spin').addClass('shrink');
+		$('#win img').attr("src", winning_prize.img);
+		$('.claimed a').attr("href", $('.claimed a').data("ref") + winning_prize.code);
+		$('#spin').addClass('shrink');
 	}, 5500);
 
-	/*
+	
 	setTimeout(() => {
 		$('#win').addClass('in');
 	}, 5750);
-	*/
-
-
 }
 
 
