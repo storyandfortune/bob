c8f3d8133da0aef94ec82332417008fe0a1d72d7
storyandfortune@github.com <fortune@storyandfortune.com>
2022-09-18 15:17:28 -0700

    cleaning up

--- assets/spin-2-win.js
+++ assets/spin-2-win.js
@@ -1,92 +1,109 @@
 
 
-	let win = [
-		{deg:0, title:"Beef Patty", img:"", code:"", ratio:0}, 
-		{deg:45, title:"25% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-25percent.png?v=1663197700", code:"WIN25%OFF", ratio:10}, 
-		{deg:90, title:"Free Stickers", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-stickers.png?v=1663197700", code:"FREESTICKER", ratio:100 },
-		{deg:135, title:"20% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-20percent.png?v=1663197699", code:"WIN20%OFF", ratio:5 },
-		{deg:180, title:"Free Postcards", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-postcards.png?v=1663197699", code:"FREEPOSTCARDS", ratio:100},
-		{deg:225, title:"10% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-10percent.png?v=1663197699", code:"WIN10%OFF", ratio:100},
-		{deg:270, title:"Free Emoji", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-emoji.png?v=1663197699", code:"FREEEMOJI", ratio:100},
-		{deg:315, title:"15% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-15percent.png?v=1663197699", code:"WIN15%OFF", ratio:100}
-	];
+	let spinToWin = {
+		win:[
+			{deg:0, title:"Beef Patty", img:"", code:"", ratio:0}, 
+			{deg:45, title:"25% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-25percent.png?v=1663197700", code:"WIN25%OFF", ratio:10}, 
+			{deg:90, title:"Free Stickers", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-stickers.png?v=1663197700", code:"FREESTICKER", ratio:100 },
+			{deg:135, title:"20% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-20percent.png?v=1663197699", code:"WIN20%OFF", ratio:5 },
+			{deg:180, title:"Free Postcards", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-postcards.png?v=1663197699", code:"FREEPOSTCARDS", ratio:100},
+			{deg:225, title:"10% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-10percent.png?v=1663197699", code:"WIN10%OFF", ratio:100},
+			{deg:270, title:"Free Emoji", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-emoji.png?v=1663197699", code:"FREEEMOJI", ratio:100},
+			{deg:315, title:"15% Off", img:"https://cdn.shopify.com/s/files/1/0593/5942/8759/files/spin2win-15percent.png?v=1663197699", code:"WIN15%OFF", ratio:100}
+		],
+		win_ratio: [],
+		coupon_code:null,
+		copy :{
+			defaultTitle:"Spin to Win",
+			defualttagline:"Spin the wheel for a chance to win!",
+			cta:"Enter your e-mail address to collect your prize."
+		},
+		prize:null,
+		winning_prize:null,
+		deg:null,
+		preload:null,
+		init(){
+			setTimeout(() => {
 
-	let win_ratio = [];
-	let el = 0;
-	win.forEach((element) => {
-		for(i=0; i<element.ratio; i++){
-			win_ratio.push(el);
-		}
-		el++;
-	});
+				this.win_ratio = [];
+				let el = 0;
+				this.win.forEach((element) => {
+					for(i=0; i<element.ratio; i++){
+						this.win_ratio.push(el);
+					}
+					el++;
+				});
 
-	let coupon_code = window.sessionStorage.getItem("coupon");
+				this.coupon_code = window.sessionStorage.getItem("coupon");
 
-	if(coupon_code){
-		let v  = JSON.parse(coupon_code);
-		$('.claimed .code').html(v.code);
-		$('.claimed a').attr("href",  $('.claimed a').data("ref") + v.code);
-	}
+				if(this.coupon_code){
+					let v  = JSON.parse(this.coupon_code);
+					$('.claimed .code').html(v.code);
+					$('.claimed a').attr("href",  $('.claimed a').data("ref") + v.code);
+				}
 
-	setTimeout(() => {
-		$('.gameboy').addClass('jump');
-	}, 750);
+				// make the big boy jump
+				$('.gameboy').addClass('jump');
+		
+				/* bind -------------------------------*/
+				$('.spin-win #spin .spin-btn').on('click', () => {
+					console.log('spin');
+					this.spin();
+				});
+				
+			}, 750);
+		},
+		spin(){
+			this.prize = (Math.floor(Math.random() * (1 - this.win_ratio.length)) + this.win_ratio.length) -1;
 
-
-let spin = function(){
-
-	let copy = {
-		defaultTitle:"Spin to Win",
-		defualttagline:"Spin the wheel for a chance to win!",
-		cta:"Enter your e-mail address to collect your prize."
-	}
-
-	let prize = (Math.floor(Math.random() * (1 - win_ratio.length)) + win_ratio.length) -1;
-	console.log(prize);
-
-	let winning_prize = win[win_ratio[prize]];
-	let x = 5; //min rotation
-	let y = 25; // max rotation
-
-	let deg = 360 * (Math.floor(Math.random() * (x - y)) + y) + winning_prize.deg;
-
-	$('#box').css("transform", "rotate(-"+deg+"deg)");
-
-	setTimeout(() => {
-		$('#title h2').html(winning_prize.title +"!");
-		$('#title span').html(copy.cta);
-		$('#win img').attr("src", winning_prize.img);
-		$('.claimed a').attr("href", $('.claimed a').data("ref") + winning_prize.code);
-		$('#spin').addClass('shrink');
-
- 		addCoupon();
-
-	}, 5500);
-
+			this.winning_prize = this.win[this.win_ratio[this.prize]];
+			let x = 5; //min rotation
+			let y = 15; // max rotation
 	
-	setTimeout(() => {
-		$('#win').addClass('in');
-	}, 5750);
-}
+			this.deg = 360 * (Math.floor(Math.random() * (x - y)) + y) + this.winning_prize.deg;
+	
+	
+			$('#box').css("transform", "rotate(-"+this.deg+"deg)");
+	
+			//preload image
+			this.preload = new Image();
+			this.preload.src = this.winning_prize.img;
+	
+	
+			setTimeout(() => {
+				$('#title h2').html(this.winning_prize.title +"!");
+				$('#title span').html(this.copy.cta);
+				$('#win img').attr("src", this.winning_prize.img);
+				$('.claimed a').attr("href", $('.claimed a').data("ref") + this.winning_prize.code);
+				$('#spin').addClass('shrink');
+	
+				this.addCoupon();
+	
+			}, 5500);
+	
+			
+			setTimeout(() => {
+				$('#win').addClass('in');
+			}, 5750);
 
-// submit e-mail.
-let addCoupon = function(){
+		},
+		addCoupon(){
 
-	window.sessionStorage.clear();
+			window.sessionStorage.clear();
 
-	let data = {
-		title: win[prize].title,
-		code:  win[prize].code,
-		time: Date.now()
-	};
+			let data = {
+				title: this.winning_prize.title,
+				code:  this.winning_prize.code,
+				time: Date.now()
+			};
+	
+			let value = JSON.stringify(data);
+			window.sessionStorage.setItem("coupon", value);
 
-	let value = JSON.stringify(data);
-	window.sessionStorage.setItem("coupon", value);
-}
+		},
+		reset(){
+			window.sessionStorage.clear();
+		}
+	}
 
-// reset game.
-let reset = function(){
-	window.sessionStorage.clear();
-}
-
-
+	spinToWin.init();
