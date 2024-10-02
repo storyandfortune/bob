d7cb3a69359342ddba444fe464d381976fe80712
rickytruth <rick@rickytruth.com>
2024-10-02 10:02:41 -0700

    fixing view height on safari mobile

--- assets/landing-hv.js
+++ assets/landing-hv.js
@@ -471,6 +471,16 @@ const app = Vue.createApp({
 	mounted(){
 		console.log('Heros & Villians')
 		document.addEventListener('keydown', this.handleKeyPress);
+
+		window.addEventListener('resize', () => {
+			// We execute the same script as before
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		  });
+
+		let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 		this.init()
 	},
 	beforeUnmount() {
